c76440106a867ed810206308554716bc82eb7aa7
xwdajtf4 <dattong140304@gmail.com>
2024-09-15 22:26:59 +0700

    Add files via upload

---
+++ fromat_true_1.asm
@@ -0,0 +1,11 @@
+.model small   <che do bo nho>
+.stack 100h
+.data        
+    <khai bao du lieu>
+.code    
+<thu tuc chinh >
+    proc 
+        <cac lenh chay chuong trinh >
+<thu tuc chinh > Endp
+    <cac thu tuc kha >
+END
